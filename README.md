85fa68df6c791528e64b3794b75b189a87c05d0d
qinacme <qinhehahaha@hotmail.com>
2017-01-12 16:26:48 +0800

    [qinacme]: Finish matplotlib api test.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # astro-physics-research
 This repo is for my bachelor graduation codes.
+
+## Notes
+1. Science over measurement
+2. PSF alignment in Fourier space by Lu/He
+3. Use mathematica draw picture
+4. galsim
+
+
+ds9 -mosaic wcs 831552p_*OFCBC.fits
+
+main.f
+field_processing
+read_info
+measure_shear
+merge_explosures
--- read_fits_file.py
+++ read_fits_file.py
@@ -2,9 +2,12 @@
 
 from astropy.io import fits
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 image_file = 'assets/star_power831555_01.fits'
 label_file = 'assets/star_info831555_01.dat'
+
 
 # star_power -> HDUList (Header Data Unit)
 # HDUObj -> .header .data
@@ -29,29 +32,47 @@ def get_ellipticity(PSF):
     q22 = 0
     for k in range(48):
         for j in range(48):
-            q11 += PSF[k, j]*(k-24)*(k-24)
-            q12 += PSF[k, j]*(k-24)*(j-24)
-            q21 += PSF[k, j]*(j-24)*(k-24)
-            q22 += PSF[k, j]*(j-24)*(j-24)
-    q11 /= normalization
-    q12 /= normalization
-    q21 /= normalization
-    q21 /= normalization
+            q11 += PSF[k, j] * (k - 24) * (k - 24)
+            q12 += PSF[k, j] * (k - 24) * (j - 24)
+            q21 += PSF[k, j] * (j - 24) * (k - 24)
+            q22 += PSF[k, j] * (j - 24) * (j - 24)
+    # q11 /= normalization
+    # q12 /= normalization
+    # q21 /= normalization
+    # q21 /= normalization
     chi1 = (q11 - q22) / (q11 + q22)
-    chi2 = q12 / (q11 + q22)
-    return [chi1, chi2]
+    chi2 = 2 * q12 / (q11 + q22)
+    chi = math.sqrt(chi1 * chi1 + chi2 * chi2)
+    return [chi1 / chi, chi2 / chi]
+
+
+def plot_ellipticities(coord, ellip):
+    num = len(coord)
+    ellip_vector = [np.array([[coord[n][0] + 15 * ellip[n][0], coord[n][1] + 15 * ellip[n][1]],
+                              [coord[n][0] - 15 * ellip[n][0], coord[n][1] - 15 * ellip[n][1]]])
+                    for n in range(num)]
+    for i in range(num):
+        vertices = ellip_vector[i]
+        color = 'r'
+        plt.plot(vertices[:,0], vertices[:,1], color=color)
+
+    plt.show()
+
 
 if __name__ == '__main__':
     label_f = open(label_file, 'r')
     labels = label_f.readlines()[1:]
     label_f.close()
-    labels = [x.split()[0:3] for x in labels]
+    labels = [x.split()[1:3] for x in labels]
+    labels = [[eval(x[0]), eval(x[1])] for x in labels]
     star_number = len(labels)
     # no. x y
-    # x 0 - 2000
-    # y 0 - 4800
+    # x 0 - 2043
+    # y 0 - 4606
     star_power = fits.open(image_file)
     star_power_data = star_power[0].data
-    ellipticities = [get_ellipticity(star_power_data[((i/15)*48):((i/15+1)*48),((i % 15)*48):((i % 15+1)*48)])
-                     for i in range(star_number)]
-
+    ellipticities = [
+        get_ellipticity(star_power_data[((i / 15) * 48):((i / 15 + 1) * 48), ((i % 15) * 48):((i % 15 + 1) * 48)])
+        for i in range(star_number)]
+    plot_ellipticities(labels, ellipticities)
+    print ellipticities
